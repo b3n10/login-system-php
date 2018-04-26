8882ea78f5df260e62572e49b0373e9495637bfd
b3n10 <francisben.lleve@gmail.com>
2018-04-26 09:13:42 +0800

    Rename info with login prefix

--- header.php
+++ header.php
@@ -10,8 +10,8 @@
 		<h2><a class="home" href="index.php">Home</a></h2>
 		<div class="login-form">
 			<form action="#" method="POST">
-				<input type="text" name="username" placeholder="Username">
-				<input type="password" name="password" placeholder="Password">
+				<input type="text" name="login_username" placeholder="Username">
+				<input type="password" name="login_password" placeholder="Password">
 				<button type="submit">Login</button>
 			</form>
 			<a class="signup" href="signup.php">Sign up</a>
