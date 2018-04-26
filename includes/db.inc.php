044f72f889fc1760a55da22a6b0a63bec34d7863
b3n10 <francisben.lleve@gmail.com>
2018-04-26 08:39:33 +0800

    Add connection to db

---
+++ includes/db.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$db_servername = "localhost";
+$db_username = "root";
+$db_password = "jairah";
+$db_name = "reg_login_db";
+
+$conn = mysqli_connect($db_servername, $db_username, $db_password, $db_name);
