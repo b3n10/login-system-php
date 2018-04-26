169c71132b065e4d3095a8bb16fdd06b1720a4f0
b3n10 <francisben.lleve@gmail.com>
2018-04-26 08:37:02 +0800

    Add header and footer

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?php echo $title; ?></title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<header>
+		<h2><a class="home" href="index.php">Home</a></h2>
+		<div class="login-form">
+			<form action="#" method="POST">
+				<input type="text" name="username" placeholder="Username">
+				<input type="password" name="password" placeholder="Password">
+				<button type="submit">Login</button>
+			</form>
+			<a class="signup" href="signup.php">Sign up</a>
+		</div>
+	</header>
