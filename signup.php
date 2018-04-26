4d51dfbfe446efe2dd290abee34036f1e0e83502
b3n10 <francisben.lleve@gmail.com>
2018-04-26 09:14:02 +0800

    Update on sign up form

--- signup.php
+++ signup.php
@@ -3,7 +3,17 @@
 	require_once "header.php";
 ?>
 <section>
-	<h1>Sign Up</h1>
+	<div class="signup-wrapper">
+		<h1>Sign Up</h1>
+		<form class="signup-form" action="includes/signup.inc.php" method="POST">
+			<input type="text" name="firstname" placeholder="Firstname">
+			<input type="text" name="lastname" placeholder="Lastname">
+			<input type="text" name="email_address" placeholder="E-mail Address">
+			<input type="text" name="username" placeholder="Username">
+			<input type="password" name="user_password" placeholder="Password">
+			<button type="submit">Submit</button>
+		</form>
+	</div>
 </section>
 <?php
 	require_once "footer.php";
