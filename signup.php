0e67f953a6750d010611724797e06b5c8b7116f2
b3n10 <francisben.lleve@gmail.com>
2018-04-26 11:34:50 +0800

    Add success message

--- signup.php
+++ signup.php
@@ -4,6 +4,20 @@
 ?>
 <section>
 	<div class="signup-wrapper">
+		<span class="error">
+			<?php
+				if (isset($_GET["error_msg"])) {
+					echo "Error: " .  $_GET["error_msg"];
+				}
+			?>
+		</span>
+		<span class="success">
+			<?php
+				if (isset($_GET["success_msg"])) {
+					echo "Success: " .  $_GET["success_msg"];
+				}
+			?>
+		</span>
 		<h1>Sign Up</h1>
 		<form class="signup-form" action="includes/signup.inc.php" method="POST">
 			<input type="text" name="firstname" placeholder="Firstname">
@@ -11,7 +25,7 @@
 			<input type="text" name="email_address" placeholder="E-mail Address">
 			<input type="text" name="username" placeholder="Username">
 			<input type="password" name="user_password" placeholder="Password">
-			<button type="submit">Submit</button>
+			<button type="submit" name="submit">Submit</button>
 		</form>
 	</div>
 </section>
