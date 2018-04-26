a13ac75c9a00abe5d4f4252be20199ebfbef8073
b3n10 <francisben.lleve@gmail.com>
2018-04-26 11:35:15 +0800

    Initial user

---
+++ add_user.sql
@@ -0,0 +1,15 @@
+USE reg_login_db;
+
+INSERT INTO users (
+	first_name,
+	last_name,
+	email_address,
+	user_name,
+	password
+) VALUES (
+	"ben",
+	"lleve",
+	"francisben.lleve@gmail.com",
+	"ben10",
+	"jairah"
+);
