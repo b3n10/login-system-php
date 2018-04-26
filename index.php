2f01576c763fde2bdfedbd6961be1432b0a501fa
b3n10 <francisben.lleve@gmail.com>
2018-04-26 08:37:22 +0800

    Add index and signup page

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+	$title = "Login Page";
+	require_once "header.php";
+?>
+<section>
+	<h1>Welcome</h1>
+</section>
+<?php
+	require_once "footer.php";
+?>
---
+++ signup.php
@@ -0,0 +1,10 @@
+<?php
+	$title = "Sign Up";
+	require_once "header.php";
+?>
+<section>
+	<h1>Sign Up</h1>
+</section>
+<?php
+	require_once "footer.php";
+?>
