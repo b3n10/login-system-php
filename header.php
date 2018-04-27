944c9fe9366cb6b31c1b8639559b5c7aec812d0e
b3n10 <francisben.lleve@gmail.com>
2018-04-27 10:11:18 +0800

    Update to if else (shorthand) for session

--- header.php
+++ header.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,11 +12,17 @@
 	<header>
 		<h2><a class="home" href="index.php">Home</a></h2>
 		<div class="login-form">
-			<form action="#" method="POST">
-				<input type="text" name="login_username" placeholder="Username">
+		<?php if (isset($_SESSION["user_name"])): ?>
+			<form action="includes/logout.inc.php" method="POST">
+				<button type="submit" name="submit">Logout</button>
+			</form>
+		<?php else: ?>
+			<form action="includes/login.inc.php" method="POST">
+				<input type="text" name="login_username" placeholder="Username/E-mail address">
 				<input type="password" name="login_password" placeholder="Password">
-				<button type="submit">Login</button>
+				<button type="submit" name="submit">Login</button>
 			</form>
 			<a class="signup" href="signup.php">Sign up</a>
+		<?php endif ?>
 		</div>
 	</header>
