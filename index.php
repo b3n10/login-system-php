f8cb141d1d6bea245b7c91a6adc6599a0ef83487
b3n10 <francisben.lleve@gmail.com>
2018-04-27 10:12:47 +0800

    Update to if else (shorthand) to view user info when logged in

--- index.php
+++ index.php
@@ -3,7 +3,26 @@
 	require_once "header.php";
 ?>
 <section>
-	<h1>Welcome</h1>
+	<?php if (isset($_SESSION["user_name"])): ?>
+		<h1>Welcome, <?php echo $_SESSION["user_name"]; ?></h1>
+		<div class="welcome-user">
+			<p>Here are your details:</p>
+			<ul>
+				<li>First name: <?php echo $_SESSION["first_name"]; ?> </li>
+				<li>Last name: <?php echo $_SESSION["last_name"]; ?> </li>
+				<li>E-mail address: <?php echo $_SESSION["email_address"]; ?> </li>
+			</ul>
+		</div>
+	<?php else: ?>
+		<h1>Index Page</h1>
+		<span class="error">
+			<?php
+				if (isset($_GET["login_msg"])) {
+					echo "Error: " .  $_GET["login_msg"];
+				}
+			?>
+		</span>
+	<?php endif ?>
 </section>
 <?php
 	require_once "footer.php";
